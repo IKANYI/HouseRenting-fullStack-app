97af2115d98791b860f2d82a90d08c7c1e825220
IKANYI <paulkaranja214@gmail.com>
2024-07-02 09:37:18 +0300

    create a homepage and hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,20 @@
+import heroImg from '../assets/hero2.jpg';
+import "./Major.css";
+function Hero(){
+  return(
+    <div className="hero-section">
+      <div className="heroImg">
+        <img src={heroImg} alt="hero image" />
+      </div>
+      <div className="hero-headline">
+        <h2>discover amazing places to live</h2>
+        <p>our houses are fully furnished with modern design to allow you experience life like never before</p>
+      </div>
+      <div className="hero-button">
+        <button className='login-button'>login</button>
+        <button className='signUp-button'>login</button>
+      </div>
+    </div>
+  )
+}
+export default Hero
---
+++ src/components/Home.jsx
@@ -0,0 +1,22 @@
+import HomeImg from '../assets/homeImg.svg'
+import './Major.css';
+function Home() {
+  return(
+    <>
+      <div className="homeImg">
+        <img src={HomeImg} alt="home image" />
+      </div>
+      <div className="headline">
+        <h2>keja yangu</h2>
+        <h3>
+          find your ideal place according to your needs, pocket and expectations
+        </h3>
+      </div>
+      <div className="homeButton">
+        <button>next</button>
+      </div>
+
+    </>
+  )
+}
+export default Home
